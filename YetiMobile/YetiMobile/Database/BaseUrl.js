fc33abb966708bcbbb505bc03cf97954ddd59d86
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-04-16 21:17:36 +0545

    initial

--- YetiMobile/YetiMobile/Database/BaseUrl.js
+++ YetiMobile/YetiMobile/Database/BaseUrl.js
@@ -1,1 +1,2 @@
-export const BaseUrl = "https://8f97-2400-1a00-b012-a9d3-9418-6731-cc51-5c73.ngrok-free.app/api/"
+export const BaseUrl =
+  "https://f815-2400-1a00-b012-a9d3-d4b9-a09c-a83e-92d6.ngrok-free.app/api/";
